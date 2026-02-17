49af33addb522cd6c90a01815a26c7471827f7ff
mdAbirDeveloper <mdabir.developer@gmail.com>
2026-02-17 22:13:56 +0600

    after add api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 const API_BASE =
-  process.env.NEXT_PUBLIC_API_BASE || "http://localhost:5000/api";
+  process.env.NEXT_PUBLIC_API_BASE || "https://gar-san-ganarator-bak-end.vercel.app/api";
 
 /* -------------------------------------------------------------------------- */
 /*                             ADMIN PIN STORAGE                              */
